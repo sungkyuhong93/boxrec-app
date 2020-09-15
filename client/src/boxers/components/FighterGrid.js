b4e7c7c348dd76a1793477cb2874b23249c0a1b7
Sung Kyu Hong <sungkyuhong93@gmail.com>
2020-09-15 00:30:06 -0400

    test

--- client/src/boxers/components/FighterGrid.js
+++ client/src/boxers/components/FighterGrid.js
@@ -3,6 +3,49 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { getProfiles } from "../../actions/profile";
+import loma from "../../assets/loma.jpg";
+import teo from "../../assets/teo.jpg";
+import shakur from "../../assets/shakur.jpg";
+import terrance from "../../assets/crawford.jpg";
+
+const FIGHTERS = [
+  {
+    id: "1",
+    name: "Vasily Lomachenko",
+    image: loma,
+    wins: "14",
+    losses: "1",
+    draws: "0",
+    kos: "10",
+  },
+  {
+    id: "2",
+    name: "Teofimo Lopez",
+    image: teo,
+    wins: "15",
+    losses: "0",
+    draws: "0",
+    kos: "12",
+  },
+  {
+    id: "3",
+    name: "Shakur Stevenson",
+    image: shakur,
+    wins: "14",
+    losses: "0",
+    draws: "0",
+    kos: "8",
+  },
+  {
+    id: "4",
+    name: "Terrance Crawford",
+    image: terrance,
+    wins: "36",
+    losses: "0",
+    draws: "0",
+    kos: "27",
+  },
+];
 
 const FighterGrid = ({ getProfiles, profile: { profiles } }) => {
   useEffect(() => {
@@ -14,39 +57,65 @@ const FighterGrid = ({ getProfiles, profile: { profiles } }) => {
       <div className="fighter-grid-inner">
         <h2 id="all">All Fighters</h2>
         <div className="fight-grid-inner-row">
-          {profiles.length > 0 ? (
-            profiles.map((profile) => {
-              return (
-                <div className="fighter" key={profile._id}>
-                  <div
-                    className="fighter-img"
-                    style={{
-                      backgroundImage: `url(${process.env.REACT_APP_ASSET_URL}/${profile.image})`,
-                    }}
-                  ></div>
+          {FIGHTERS.map((fighter) => {
+            return (
+              <div className="fighter" key={fighter.id}>
+                <div
+                  className="fighter-img"
+                  style={{
+                    backgroundImage: `url(${fighter.image})`,
+                  }}
+                ></div>
 
-                  <div className="fighter-info">
-                    <div className="fighter-info-record">
-                      <p>{profile.user.name}</p>
-                      <p>{profile.wins} Wins</p>
-                      <p>{profile.losses} Losses</p>
-                      <p>{profile.draws} Draws</p>
-                      <p>{profile.kos} KOS</p>
+                <div className="fighter-info">
+                  <div className="fighter-info-record">
+                    <p>{fighter.name}</p>
+                    <p>{fighter.wins} Wins</p>
+                    <p>{fighter.losses} Losses</p>
+                    <p>{fighter.draws} Draws</p>
+                    <p>{fighter.kos} KOS</p>
 
-                      <Link
-                        to={`/profile/${profile._id}`}
-                        className="sk-btn primary-btn-flip view-profile"
-                      >
-                        View Profile
-                      </Link>
-                    </div>
+                    <Link
+                      to={`/profile/${fighter.id}`}
+                      className="sk-btn primary-btn-flip view-profile"
+                    >
+                      View Profile
+                    </Link>
                   </div>
                 </div>
-              );
-            })
-          ) : (
-            <h3>No Profiles Found</h3>
-          )}
+              </div>
+            );
+          })}
+
+          {profiles.map((profile) => {
+            return (
+              <div className="fighter" key={profile._id}>
+                <div
+                  className="fighter-img"
+                  style={{
+                    backgroundImage: `url(${process.env.REACT_APP_ASSET_URL}/${profile.image})`,
+                  }}
+                ></div>
+
+                <div className="fighter-info">
+                  <div className="fighter-info-record">
+                    <p>{profile.user.name}</p>
+                    <p>{profile.wins} Wins</p>
+                    <p>{profile.losses} Losses</p>
+                    <p>{profile.draws} Draws</p>
+                    <p>{profile.kos} KOS</p>
+
+                    <Link
+                      to={`/profile/${profile._id}`}
+                      className="sk-btn primary-btn-flip view-profile"
+                    >
+                      View Profile
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
--- client/src/boxers/templates/BoxerProfile.js
+++ client/src/boxers/templates/BoxerProfile.js
@@ -3,6 +3,57 @@ import { useParams } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { getProfiles } from "../../actions/profile";
+import loma from "../../assets/loma.jpg";
+import teo from "../../assets/teo.jpg";
+import shakur from "../../assets/shakur.jpg";
+import terrance from "../../assets/crawford.jpg";
+
+const FIGHTERS = [
+  {
+    id: "1",
+    name: "Vasily Lomachenko",
+    image: loma,
+    wins: "14",
+    losses: "1",
+    draws: "0",
+    kos: "10",
+    dob: "02-17-1988",
+    birthPlace: "Bilhorod-Dnistrovskyi, Ukraine",
+  },
+  {
+    id: "2",
+    name: "Teofimo Lopez",
+    image: teo,
+    wins: "15",
+    losses: "0",
+    draws: "0",
+    kos: "12",
+    dob: "07-30-1997",
+    birthPlace: "Brooklyn, New York",
+  },
+  {
+    id: "3",
+    name: "Shakur Stevenson",
+    image: shakur,
+    wins: "14",
+    losses: "0",
+    draws: "0",
+    kos: "8",
+    dob: "06-28-1997",
+    birthPlace: "Newark, New Jersey",
+  },
+  {
+    id: "4",
+    name: "Terrance Crawford",
+    image: terrance,
+    wins: "36",
+    losses: "0",
+    draws: "0",
+    kos: "27",
+    ob: "09-29-1987",
+    birthPlace: "Omaha, Nebraska",
+  },
+];
 
 const BoxerProfile = ({ getProfiles, profile: { profiles } }) => {
   const profileId = useParams().id;
@@ -12,10 +63,15 @@ const BoxerProfile = ({ getProfiles, profile: { profiles } }) => {
   const loadedProfile = profiles.find((profile) => {
     return profile._id == profileId;
   });
+
+  const loadedFighter = FIGHTERS.find((fighter) => {
+    return fighter.id == profileId;
+  });
+
   console.log("loaded profile");
   console.log(loadedProfile);
 
-  return (
+  return loadedProfile != null ? (
     <div className="boxer-profile">
       <div className="boxer-profile-inner site-padding">
         <div className="boxer-profile-img">
@@ -32,6 +88,24 @@ const BoxerProfile = ({ getProfiles, profile: { profiles } }) => {
           <p>KOS: {loadedProfile.kos}</p>
           <p>Date of Birth: {loadedProfile.dob}</p>
           <p>Birth Place: {loadedProfile.birthPlace}</p>
+        </div>
+      </div>
+    </div>
+  ) : (
+    <div className="boxer-profile">
+      <div className="boxer-profile-inner site-padding">
+        <div className="boxer-profile-img">
+          <img src={`${loadedFighter.image}`} />
+        </div>
+
+        <div className="boxer-profile-info">
+          <p>Name: {loadedFighter.name}</p>
+          <p>Wins: {loadedFighter.wins}</p>
+          <p>Losses: {loadedFighter.losses}</p>
+          <p>Draws: {loadedFighter.draws}</p>
+          <p>KOS: {loadedFighter.kos}</p>
+          <p>Date of Birth: {loadedFighter.dob}</p>
+          <p>Birth Place: {loadedFighter.birthPlace}</p>
         </div>
       </div>
     </div>
