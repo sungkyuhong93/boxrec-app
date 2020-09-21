5872514922b823d2cbbca04adbc50bd07422212b
combo-nadia <nadia@combo.nyc>
2020-09-20 22:53:11 -0400

    test

---
+++ client/build/precache-manifest.25ccbc68b153edf23d1ede4ba7b0ce6a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b37b34199fda86c36d8f750dcecb886",
+    "url": "/index.html"
+  },
+  {
+    "revision": "609ed2f32a2063cd5c21",
+    "url": "/static/css/main.2fffe66b.chunk.css"
+  },
+  {
+    "revision": "079ee9e4cf724ae130f3",
+    "url": "/static/js/2.386020d0.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/static/js/2.386020d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "609ed2f32a2063cd5c21",
+    "url": "/static/js/main.6e0ce4ea.chunk.js"
+  },
+  {
+    "revision": "893dcede70680dff3c67",
+    "url": "/static/js/runtime-main.31cf0e6b.js"
+  },
+  {
+    "revision": "c9db527c57f13336e0fdcb3525aaf3b8",
+    "url": "/static/media/crawford.c9db527c.jpg"
+  },
+  {
+    "revision": "aaed28fb463e8fc754bd7b42cfc5d2b6",
+    "url": "/static/media/hero.aaed28fb.jpg"
+  },
+  {
+    "revision": "6a18cd175b5b5b2ed6ff29862721197c",
+    "url": "/static/media/loma.6a18cd17.jpg"
+  },
+  {
+    "revision": "141d5c79e8930c783689039447400616",
+    "url": "/static/media/teo.141d5c79.jpg"
+  }
+]);
